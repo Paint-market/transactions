f7fb1a6158d68afd65b94cb63fd78ac04eabf03a
Piet Geursen <pietgeursen@gmail.com>
2016-03-10 17:32:37 +1300

    Get server running

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', routes);
-app.use('/transactions', transactions);
+//app.use('/transactions', transactions);
 
 //
 
