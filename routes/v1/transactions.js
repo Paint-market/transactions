728097259a3befa2e4806a906ed83ffc6cf292a7
Daniel de la Cruz & Lucy Moore <pair+Danieldelacruz01.LucyMoore@devbootcamp.com>
2016-03-11 15:59:50 +1300

    add write file function, confirmed working

--- routes/v1/transactions.js
+++ routes/v1/transactions.js
@@ -1,70 +1,90 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs')
 
-var transactions = {
-  "transactions": [
-    {
-      "buyerID": 1,
-      "ownerID": 3,
-      "paintingID": 1,
+var transactions = {}
 
-    },
-    {
-      "buyerID": 3,
-      "ownerID": 2,
-      "paintingID": 3,
-      "price": 50
-    },
-    {
-      "buyerID": 1,
-      "ownerID": 3,
-      "paintingID": 3,
-      "price": 200
-    },
-    {
-      "buyerID": 3,
-      "ownerID": 5,
-      "paintingID": 9,
-      "price": 50
-    }
-]}
+// var transactions = {
+//   "transactions": [
+//     {
+//       "buyerID": 1,
+//       "ownerID": 3,
+//       "paintingID": 1,
 
-/* GET users listing. */
+//     },
+//     {
+//       "buyerID": 3,
+//       "ownerID": 2,
+//       "paintingID": 3,
+//       "price": 50
+//     },
+//     {
+//       "buyerID": 1,
+//       "ownerID": 3,
+//       "paintingID": 3,
+//       "price": 200
+//     },
+//     {
+//       "buyerID": 3,
+//       "ownerID": 5,
+//       "paintingID": 9,
+//       "price": 50
+//     }
+// ]}
+
+/* GET transactions list. */
 router.get('/', function(req, res, next) {
-  var transactionResult = {}
-  var searchParams = Object.keys(req.query)
-  if (searchParams.length === 0){
-    transactionResult = transactions
-  } else {
-    var filteredResults = []
-    transactions.transactions.forEach(function(transaction){
-      for (var i = 0; i < searchParams.length; i++){
-        if(transaction[searchParams[i]] === Number(req.query[searchParams[i]])){
-          filteredResults.push(transaction)
+  //read object in from file
+  fs.readFile('DB.json','utf8', function(err, data){
+    transactions = JSON.parse(data)
+
+    var transactionResult = {}
+    var searchParams = Object.keys(req.query)
+    if (searchParams.length === 0){
+      transactionResult = transactions
+    } else {
+      var filteredResults = []
+      transactions.transactions.forEach(function(transaction){
+        for (var i = 0; i < searchParams.length; i++){
+          if(transaction[searchParams[i]] === Number(req.query[searchParams[i]])){
+            filteredResults.push(transaction)
+          }
         }
-      }
-    })
-    transactionResult["transactions"] = filteredResults
+      })
+      transactionResult["transactions"] = filteredResults
+    }
+    if(transactionResult.transactions.length == 0){
+      res.status(404).send('invalid search parameters')
+    }
+    else {
+      res.json(transactionResult);
   }
-  res.json(transactionResult);
+  })
+
 });
 
 
 /* POST new listing*/
 router.post('/', function(req, res, next) {
-  var newTransaction = req.body
-  var viable = true;
-  for (var property in newTransaction) {
-    if (isNaN(newTransaction[property]) || typeof newTransaction[property] != 'number') {
-      viable = false
-      res.status(404).send('This transaction has wrong ' + property + ' value. Value entered is ' + newTransaction[property])
+  fs.readFile('DB.json','utf8', function(err, data){
+    transactions = JSON.parse(data)
+    var newTransaction = req.body
+    var viable = true;
+    for (var property in newTransaction) {
+      if (isNaN(newTransaction[property]) || typeof newTransaction[property] != 'number') {
+        viable = false
+        res.status(404).send('This transaction has wrong ' + property + ' value. Value entered is ' + newTransaction[property])
+      }
     }
-  }
-  if (viable){
-  newTransaction["transactionID"] = transactions.transactions.length+1
-  transactions.transactions.push(newTransaction)
-  res.json(newTransaction)
-  }
+    if (viable){
+    newTransaction["transactionID"] = transactions.transactions.length+1
+    transactions.transactions.push(newTransaction)
+    //write file?
+    fs.writeFile('DB.json',JSON.stringify(transactions))
+
+    res.json(newTransaction)
+    }
+  })
 });
 
 
